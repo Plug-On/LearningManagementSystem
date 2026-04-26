6897b9dfcfb1866b7cef2c8d970b108a44e17063
Plug-On <plugon035@gmail.com>
2026-04-27 01:08:12 +0545

    frontend homepage

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Layout from '../common/Layout'
+import Hero from '../common/Hero'
+import FeaturedCategories from '../common/FeaturedCategories'
+import FeaturedCourses from '../common/FeaturedCourses'
 
 const Home = () => {
   return (
     <div>
         
-        <h1>Home</h1>
+        <Layout>
+          <Hero/>
+          <FeaturedCategories/>
+          <FeaturedCourses/>
+        </Layout>
 
     </div>
   )
